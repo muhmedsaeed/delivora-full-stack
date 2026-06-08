ea507823b90dd9171cccbce813028929f7798d36
muhmedsaeed <mohamed.saied.5755@gmail.com>
2026-06-09 00:59:07 +0300

    added food review controller.

---
+++ backend/Delivora/Controllers/FoodReviewController.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace Delivora.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FoodReviewController : ControllerBase
+{
+    private readonly UnitOfWorks _unitOfWorks;
+    private readonly IMapper _mapper;
+
+    public FoodReviewController(UnitOfWorks uow, IMapper mapper)
+    {
+        _unitOfWorks = uow;
+        _mapper = mapper;
+    }
+
+
+    [HttpGet("food/{foodId:int}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetByFood(int foodId)
+    {
+        var reviews = await _unitOfWorks.FoodReviewRepository.GetAllAsync();
+
+        return Ok(_mapper.Map<List<FoodReviewDto>>(reviews.Where(r => r.FoodId == foodId)));
+    }
+
+
+    [HttpPost]
+    [Authorize(Roles = "Customer")]
+    public async Task<IActionResult> Create([FromBody] CreateFoodReviewDto dto)
+    {
+        if (!ModelState.IsValid)
+            return ValidationProblem(ModelState);
+        
+        var review = _mapper.Map<FoodReview>(dto);
+        review.CustomerId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        
+        await _unitOfWorks.FoodReviewRepository.AddAsync(review);
+        await _unitOfWorks.SaveChangesAsync();
+        
+        return Ok(_mapper.Map<FoodReviewDto>(review));
+    }
+}
