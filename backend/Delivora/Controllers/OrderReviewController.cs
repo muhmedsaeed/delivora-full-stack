55082048497b9ec2988589d5abc47bc6fcef5a67
muhmedsaeed <mohamed.saied.5755@gmail.com>
2026-06-09 00:53:48 +0300

    added order review controller.

---
+++ backend/Delivora/Controllers/OrderReviewController.cs
@@ -0,0 +1,68 @@
+﻿
+namespace Delivora.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(Roles = "Customer")]
+public class OrderReviewController : ControllerBase
+{
+    private readonly UnitOfWorks _unitOfWorks;
+    private readonly IMapper _mapper;
+
+    public OrderReviewController(UnitOfWorks uow, IMapper mapper)
+    {
+        _unitOfWorks = uow;
+        _mapper = mapper;
+    }
+
+
+    private int GetUserId()
+    {
+        return int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+    }
+
+
+    [HttpGet("order/{orderId:int}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetByOrder(int orderId)
+    {
+        var reviews = await _unitOfWorks.OrderReviewRepository.GetAllAsync();
+
+        var filtered = reviews.Where(r => r.OrderId == orderId);
+        
+        return Ok(_mapper.Map<List<OrderReviewDto>>(filtered));
+    }
+
+
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+    [FromBody] CreateOrderReviewDto dto)
+    {
+        if (!ModelState.IsValid)
+            return ValidationProblem(ModelState);
+
+        var order = await _unitOfWorks.OrderRepository.GetByIdAsync(dto.OrderId);
+
+        if (order is null || order.CustomerId != GetUserId())
+            return BadRequest(new { message = "Invalid order." });
+
+        if (order.Status != OrderStatus.Delivered)
+            return BadRequest(new
+            {
+                message = "Order must be delivered."
+            });
+        
+        var review = _mapper.Map<OrderReview>(dto);
+        
+        review.CustomerId = GetUserId();
+        
+        await _unitOfWorks.OrderReviewRepository.AddAsync(review);
+        await _unitOfWorks.SaveChangesAsync();
+        
+        return Ok(_mapper.Map<OrderReviewDto>(review));
+    }
+
+
+}
